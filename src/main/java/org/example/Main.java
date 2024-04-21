8547da1f67e8136fa99518d5299dc3d37e8b541c
CRTXScyNet <CRTXScyNet@gmail.com>
2024-04-21 20:48:09 +0500

    HW-8 to check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello world!");
 
         // Task one
-        int[] ints = new int[]{1,2,3};
+        int[] ints = new int[]{1, 2, 3};
         double[] doubles = {1.57, 7.654, 9.986};
         String[] string = new String[4];
         string[0] = "Java";
@@ -18,44 +18,44 @@ public class Main {
         // Task two
         for (int i = 0; i < ints.length; i++) {
             System.out.print(ints[i]);
-            if (i< ints.length-1){
+            if (i < ints.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
         for (int i = 0; i < doubles.length; i++) {
             System.out.print(doubles[i]);
-            if (i< doubles.length-1){
+            if (i < doubles.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
         for (int i = 0; i < string.length; i++) {
             System.out.print(string[i]);
-            if (i< string.length-1){
+            if (i < string.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
 
         // Task three
-        for (int i = ints.length-1; i >= 0; i--) {
+        for (int i = ints.length - 1; i >= 0; i--) {
             System.out.print(ints[i]);
-            if (i > 0){
+            if (i > 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = doubles.length-1; i >= 0; i--) {
+        for (int i = doubles.length - 1; i >= 0; i--) {
             System.out.print(doubles[i]);
-            if (i > 0){
+            if (i > 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = string.length-1; i >= 0; i--) {
+        for (int i = string.length - 1; i >= 0; i--) {
             System.out.print(string[i]);
-            if (i > 0){
+            if (i > 0) {
                 System.out.print(", ");
             }
         }
@@ -63,7 +63,7 @@ public class Main {
 
         //Task four
         for (int i = 0; i < ints.length; i++) {
-            if (ints[i]%2!=0){
+            if (ints[i] % 2 != 0) {
                 ints[i] = ints[i] + 1;
             }
         }
